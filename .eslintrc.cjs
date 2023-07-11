71696be923e69fe845037489b90e3c743dd28634
Volodymyr Liutkin <volodymyr@liutkin.com>
2023-07-11 21:12:40 +0300

    feat: add tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,17 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
 }
